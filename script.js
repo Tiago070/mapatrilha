17a4716724ab2a466835616f2d5fb02c525b46d7
Tiago070 <115472816+Tiago070@users.noreply.github.com>
2022-11-17 16:25:22 -0300

    Update script.js

--- script.js
+++ script.js
@@ -1,7 +1,91 @@
 function abrircred(){
     document.getElementById("creditos").style.display = "block";
 }
-
 function fecharcred(){
     document.getElementById("creditos").style.display = "none";
+}
+
+
+
+function abririmg(){
+    document.getElementById("img_fab").style.display = "block";
+}
+function fecharimg(){
+    document.getElementById("img_fab").style.display = "none";
+}
+
+
+
+function abrirIF(){
+    document.getElementById("img_IF").style.display = "block";
+}
+function fecharIF(){
+    document.getElementById("img_IF").style.display = "none";
+}
+
+
+
+function abrirCE(){
+    document.getElementById("img_CE").style.display = "block";
+}
+function fecharCE(){
+    document.getElementById("img_CE").style.display = "none";
+}
+
+
+
+function abrirequo(){
+    document.getElementById("img_equo").style.display = "block";
+}
+function fecharequo(){
+    document.getElementById("img_equo").style.display = "none";
+}
+
+
+
+function abriragroind(){
+    document.getElementById("img_agroind").style.display = "block";
+}
+function fecharagroind(){
+    document.getElementById("img_agroind").style.display = "none";
+}
+
+
+function abrirp5(){
+    document.getElementById("img_p5").style.display = "block";
+}
+function fecharp5(){
+    document.getElementById("img_p5").style.display = "none";
+}
+
+
+function abrirp1(){
+    document.getElementById("img_p1").style.display = "block";
+}
+function fecharp1(){
+    document.getElementById("img_p1").style.display = "none";
+}
+
+
+function abrirp2(){
+    document.getElementById("img_p2").style.display = "block";
+}
+function fecharp2(){
+    document.getElementById("img_p2").style.display = "none";
+}
+
+
+function abrirp4(){
+    document.getElementById("img_p4").style.display = "block";
+}
+function fecharp4(){
+    document.getElementById("img_p4").style.display = "none";
+}
+
+
+function abrirp3(){
+    document.getElementById("img_p3").style.display = "block";
+}
+function fecharp3(){
+    document.getElementById("img_p3").style.display = "none";
 }
